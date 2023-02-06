50b34600473804a59858105fec5c593d16eb55b3
1CAMOU <mgmt.camou@gmail.com>
2023-02-06 19:44:28 +0100

    fix: warnings

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,6 @@ createInertiaApp({
         return pages[`./Pages/${name}.svelte`]
     },
     setup({ el, App, props }) {
-        new App({ target: el, props })
+        new App({ target: el, props, })
     },
 })
